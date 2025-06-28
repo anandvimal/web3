f5e5ef7f7dcb19c5a703ca6045847e14b51d2e26
anandvimal <deepaksandhu789@gmail.com>
2025-06-28 14:22:20 -0700

    ch11 l2

--- solidity/Solidity: Beginner to Intermediate Smart Contracts/lesson2/zombiefeeding.sol
+++ solidity/Solidity: Beginner to Intermediate Smart Contracts/lesson2/zombiefeeding.sol
@@ -2,23 +2,26 @@ pragma solidity >=0.5.0 <0.6.0;
 
 import "./zombiefactory.sol";
 
-// Create KittyInterface here
-contract KittyInterface{
-    function getKitty(uint256 _id) external view returns (
-        bool isGestating,
-        bool isReady,
-        uint256 cooldownIndex,
-        uint256 nextActionAt,
-        uint256 siringWithId,
-        uint256 birthTime,
-        uint256 matronId,
-        uint256 sireId,
-        uint256 generation,
-        uint256 genes
-    );
+contract KittyInterface {
+  function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+  );
 }
 
 contract ZombieFeeding is ZombieFactory {
+
+  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+  // Initialize kittyContract here using `ckAddress` from above
+  KittyInterface kittyContract = KittyInterface(ckAddress);
 
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
     require(msg.sender == zombieToOwner[_zombieId]);
@@ -29,4 +32,3 @@ contract ZombieFeeding is ZombieFactory {
   }
 
 }
-
