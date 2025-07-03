6e6354726b0eb9de1c5880c805e927e12ab446a3
anandvimal <deepaksandhu789@gmail.com>
2025-07-03 14:19:28 -0700

    ch10 l3

--- solidity/Solidity: Beginner to Intermediate Smart Contracts/lesson3/zombiehelper.sol
+++ solidity/Solidity: Beginner to Intermediate Smart Contracts/lesson3/zombiehelper.sol
@@ -9,15 +9,19 @@ contract ZombieHelper is ZombieFeeding {
     _;
   }
 
-  // Start here
-  function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId){
+  function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
     require(msg.sender == zombieToOwner[_zombieId]);
     zombies[_zombieId].name = _newName;
   }
 
-  function changeDna(uint _zombieId, uint _newDna)external aboveLevel(20, _zombieId){
+  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
     require(msg.sender == zombieToOwner[_zombieId]);
     zombies[_zombieId].dna = _newDna;
+  }
+
+  // Create your function here
+  function getZombiesByOwner(address _owner) external view returns(uint[] memory){
+
   }
 
 }
