4178d1eafdc0322970571df8cfbd6d9164fb1b17
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-07-29 22:03:38 -0700

    import keys example

--- solana/Introduction to cryptography and Solana clients/generate-keypair/generate-keypair.ts
+++ solana/Introduction to cryptography and Solana clients/generate-keypair/generate-keypair.ts
@@ -1,6 +1,14 @@
-import { Keypair } from "@solana/web3.js";
-const keypair = Keypair.generate();
+import "dotenv/config";
+// import { Keypair } from "@solana/web3.js";
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
 
-console.log('The public key is: ', keypair.publicKey.toBase58());
-console.log('The secret key is: ', keypair.secretKey);
-console.log(`✅ Generated keypair!`);
+// const keypair = Keypair.generate();
+
+// console.log('The public key is: ', keypair.publicKey.toBase58());
+// console.log('The secret key is: ', keypair.secretKey);
+
+const keypair = getKeypairFromEnvironment("SECRET_KEY");
+
+console.log(
+  `✅ Finished! We've loaded our secret key securely, using an env file!`,
+);
