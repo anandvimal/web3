1a793ea38a07547d06b1b9a8c306d1cb7e781e72
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-07-30 21:46:25 -0700

    Create check-balance.ts

---
+++ solana/Introduction to cryptography and Solana clients/check-balance/check-balance.ts
@@ -0,0 +1,9 @@
+import { Connection, LAMPORTS_PER_SOL, PublicKey} from "@solana/web3.js";
+
+const publicKey = new PublicKey("DZiqa7he7jxc647PrgVxawA1L2GeJ74b8gHavaswhfGp");
+const connection = new Connection("https://api.devnet.solana.com", "confirmed");
+const balanceInLamports = await connection.getBalance(publicKey);
+
+const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
+
+console.log("Finished! the balance for the wallet at address ${publickey} is ${balanceInSOL}!")
