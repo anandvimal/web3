2451b32de87dd313402e6d7fa5d922f4721ca6a1
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-07-30 21:49:37 -0700

    fix print statement

--- solana/Introduction to cryptography and Solana clients/check-balance/check-balance.ts
+++ solana/Introduction to cryptography and Solana clients/check-balance/check-balance.ts
@@ -1,9 +1,13 @@
 import { Connection, LAMPORTS_PER_SOL, PublicKey} from "@solana/web3.js";
 
 const publicKey = new PublicKey("DZiqa7he7jxc647PrgVxawA1L2GeJ74b8gHavaswhfGp");
+
 const connection = new Connection("https://api.devnet.solana.com", "confirmed");
+
 const balanceInLamports = await connection.getBalance(publicKey);
 
 const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
 
-console.log("Finished! the balance for the wallet at address ${publickey} is ${balanceInSOL}!")
+console.log(
+  `💰 Finished! The balance for the wallet at address ${publicKey} is ${balanceInSOL}!`
+);
