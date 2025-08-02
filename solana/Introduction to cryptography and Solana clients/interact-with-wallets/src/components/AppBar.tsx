ffe45b1583edf9e9fe85f6cf00fa3a771ae3c543
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-08-02 09:47:42 -0700

    fix logo

--- solana/Introduction to cryptography and Solana clients/interact-with-wallets/src/components/AppBar.tsx
+++ solana/Introduction to cryptography and Solana clients/interact-with-wallets/src/components/AppBar.tsx
@@ -7,7 +7,7 @@ import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 export const AppBar: FC = () => {
   return (
     <div className="flex items-center justify-between p-4 bg-gray-800 w-full">
-      <Image src="/solanaLogo.png" height={30} width={200} alt="Solana Logo" />
+      <Image src="/solanaLogo.png" height={200} width={200} alt="Solana Logo" />
       <span>Wallet-Adapter Example</span>
       <WalletMultiButton />
     </div>
