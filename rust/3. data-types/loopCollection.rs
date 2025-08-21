7132b56c3e8bfddd0dbdf1bf5718580e2d632b2e
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-08-21 12:06:36 -0700

    Create loopCollection.rs

---
+++ rust/3. data-types/loopCollection.rs
@@ -0,0 +1,10 @@
+fn main(){
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while value in a.iter() {
+        println!("the value is: {}", value);
+
+        index += 1;
+    }
+}
