16fefdcd0ae599bf53f6e65bd53af57a9a1d038e
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-09-06 06:30:14 -0700

    add guess struct impl

--- rust/11.WritingAutomatedTests/adder/src/lib.rs
+++ rust/11.WritingAutomatedTests/adder/src/lib.rs
@@ -11,6 +11,19 @@ pub fn greeting(name: &str) -> String {
     //String::from("Hello")  //to test fail message use this
 }
 
+pub struct Guess{
+    value: i32,
+}
+
+impl Guess{
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {value}.")
+        }
+
+        Guess { value }
+    }
+}
 
 #[derive(Debug)]
 struct Rectangle {
