de4bf9347b07639d2ffcddcf20b3b8efc7808a6d
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-09-06 04:46:59 -0700

    Update lib.rs

--- rust/11.WritingAutomatedTests/adder/src/lib.rs
+++ rust/11.WritingAutomatedTests/adder/src/lib.rs
@@ -56,6 +56,12 @@ mod tests {
         assert_eq!(result, 4);
     }
 
+    #[test]
+    fn it_adds_two(){
+        let result = add_two(2);
+        assert_eq!(result, 4);
+    }
+
     // #[test]
     // fn another(){
     //     panic!("Make this test fail");
