7d67d8e2e1461a5c734add0183535f8f7568fbe5
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-09-07 03:08:47 -0700

    parse config function

--- rust/IO cmdline program/minigrep/src/main.rs
+++ rust/IO cmdline program/minigrep/src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
     dbg!(&args);
-  
+    
     let query = &args[1];
     let file_path = &args[2];
 
@@ -16,4 +16,11 @@ fn main() {
 
     println!("With text:\n{contents}");
     
+}
+
+fn parse_config(args: &[String]) -> (&str, &str){
+    let query = &args[1];
+    let file_path = &args[2];
+
+    (query, file_path)
 }
