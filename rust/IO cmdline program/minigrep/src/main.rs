460bf7ae7acde46b9604a915c05309e4cef5b0ef
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-09-07 17:58:14 -0700

    use Config constructor

--- rust/IO cmdline program/minigrep/src/main.rs
+++ rust/IO cmdline program/minigrep/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     dbg!(&args);
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for : {}",config.query);
     println!("In file : {}",config.file_path);
@@ -31,9 +31,3 @@ impl Config{
     }
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-
-    Config {query, file_path}
-}
