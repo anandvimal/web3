b6971caeefc4c99fa4d06edf1a7cff48f01adf57
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-09-07 20:43:55 -0700

    Update main.rs

--- rust/IO cmdline program/minigrep/src/main.rs
+++ rust/IO cmdline program/minigrep/src/main.rs
@@ -22,6 +22,8 @@ fn main() {
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
     println!("With text: \n{contents}");
+
+    Ok(())
 }
 
 struct Config {
