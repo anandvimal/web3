954bab0353de2afbba0295d062f29d679336ff52
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-10-15 15:12:05 -0700

    Update 10-13.rs

--- rust/10 generic traits lifetimes/traits/10-13.rs
+++ rust/10 generic traits lifetimes/traits/10-13.rs
@@ -1,6 +1,11 @@
 // slight changes from book version. But works as same.
 pub trait Summary {
-    fn summarize(&self) -> String;
+    fn summarize_author(&self) -> String;
+    
+    fn summarize(&self) -> String{
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+    
 }
 
 
@@ -12,8 +17,8 @@ pub struct NewsArticle {
 }
 
 impl Summary for NewsArticle {
-    fn summarize(&self) -> String {
-        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.author)
     }
 }
 
@@ -25,8 +30,8 @@ pub struct SocialPost {
 }
 
 impl Summary for SocialPost {
-    fn summarize(&self) -> String {
-        format!("{}: {}", self.username, self.content)
+    fn summarize_author(&self) -> String {
+        format!(" @{}", self.username)
     }
 }
 
