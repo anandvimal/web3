d96685c58a49e9d6d281dc7cb2b219eddb803f97
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-10-22 11:14:10 -0700

    Create 15-19.rs

---
+++ rust/15 smart pointers/15-19.rs
@@ -0,0 +1,21 @@
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
+
+fn main() {
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a)); // 1
+
+    let _b = Cons(3, Rc::clone(&a));
+    println!("Count after creating b = {}", Rc::strong_count(&a)); // 2
+
+    {
+        let _c = Cons(4, Rc::clone(&a));
+        println!("Count after creating c = {}", Rc::strong_count(&a)); // 3
+    }
+    println!("Count after c goes out of scope = {}", Rc::strong_count(&a)); // 2
+}
