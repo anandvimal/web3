e88b7e76c5b2d180846add84d39e928c7655f71c
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-10-23 11:42:30 -0700

    Create 16-11.rs

---
+++ rust/16 Fearless Concurrency/16-11.rs
@@ -0,0 +1,41 @@
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+    let tx1 = tx.clone();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi 1"),
+            String::from("from 1"),
+            String::from("the 1"),
+            String::from("thread 1"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi 2"),
+            String::from("from 2"),
+            String::from("the 2"),
+            String::from("thread 2"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
