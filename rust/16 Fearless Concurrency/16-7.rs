49f31fb8cb25dae25d7d7c700b9010ed056fd8fd
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-10-22 22:01:28 -0700

    Create 16-7.rs

---
+++ rust/16 Fearless Concurrency/16-7.rs
@@ -0,0 +1,12 @@
+use std::sync::mpsc;
+// mpsc stands for multiple producer, single consumer!.
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
+}
+
