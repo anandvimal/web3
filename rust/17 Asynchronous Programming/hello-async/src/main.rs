4d056403a411effc958fb824c327799a40fa671d
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-10-30 11:21:55 -0700

    17.2 refactor

--- rust/17 Asynchronous Programming/hello-async/src/main.rs
+++ rust/17 Asynchronous Programming/hello-async/src/main.rs
@@ -1,8 +1,7 @@
 use trpl::Html;
 
 async fn page_title(url: &str) -> Option<String> {
-    let response = trpl::get(url).await;
-    let response_text = response.text().await;
+    let response_text = trpl::get(url).await.text().await;
     Html::parse(&response_text)
     .select_first("title")
     .map(|title| title.inner_html())
