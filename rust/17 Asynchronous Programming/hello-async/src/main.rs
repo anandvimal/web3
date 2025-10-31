7f5bba1b566c6fc92087b1ad08c9adcaf9b8f2dc
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-10-30 19:40:19 -0700

    17-5

--- rust/17 Asynchronous Programming/hello-async/src/main.rs
+++ rust/17 Asynchronous Programming/hello-async/src/main.rs
@@ -1,22 +1,35 @@
-use trpl::Html;
+extern crate trpl; // required for mdbook test
+use trpl::{Either, Html};
 
-async fn page_title(url: &str) -> Option<String> {
+async fn page_title(url: &str) -> (&str, Option<String>) {
     let response_text = trpl::get(url).await.text().await;
-    Html::parse(&response_text)
-    .select_first("title")
-    .map(|title| title.inner_html())
+    let title = Html::parse(&response_text)
+        .select_first("title")
+        .map(|title| title.inner_html());
+    (url, title)
 }
 
-// this fails to compile because main is not async
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
     trpl::run(async {
-        let url = &args[1];
-        match page_title(url).await {
-            Some(title) => println!("The title for {url} is {title}"),
-            None => println!("{url} had no title"),
+        
+        let title_fut_1 = page_title(&args[1]);
+        let title_fut_2 = page_title(&args[2]);
+
+        let (url, maybe_title) = 
+            match trpl::race(title_fut_1, title_fut_2).await {
+                Either::Left(left) => left,
+                Either::Right(right) => right,
+            };
+
+        println!("{url} returned first");
+        match maybe_title {
+            Some(title) => println!("The page title is {title}"),
+            None => println!("It had no title"),
         }
     })
 }
-// cargo run -- https://www.rust-lang.org
+// cargo run -- https://www.rust-lang.org https://www.example.com
+// Listing 17-5
