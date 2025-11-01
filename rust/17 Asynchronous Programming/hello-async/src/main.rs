615e08db9ade08af879e68b6659a7ddaa9748579
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-11-01 08:33:16 -0700

    remove async block from around either of them. (second one in this case)

--- rust/17 Asynchronous Programming/hello-async/src/main.rs
+++ rust/17 Asynchronous Programming/hello-async/src/main.rs
@@ -12,13 +12,11 @@ fn main() {
         };
         fut1.await;
 
-        let fut2 = async {
-            for i in 1..5 {
-                println!("hi number {i} from second task!");
-                trpl::sleep(Duration::from_millis(500)).await;
-            }   
-        };
-        fut2.await;
+
+        for i in 1..5 {
+            println!("hi number {i} from second task!");
+            trpl::sleep(Duration::from_millis(500)).await;
+        }   
 
         //trpl::join(fut1, fut2).await;
     
