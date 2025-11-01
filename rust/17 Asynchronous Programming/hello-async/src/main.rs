e39aba85619b852367a9e8f1aef7405b1f58a282
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-11-01 09:37:17 -0700

    17-10 reciever stays open

--- rust/17 Asynchronous Programming/hello-async/src/main.rs
+++ rust/17 Asynchronous Programming/hello-async/src/main.rs
@@ -1,16 +1,29 @@
 extern crate trpl; // required for mdbook test
 
+use std::time::Duration;
+
 fn main() {
     trpl::run(async {
         let (tx, mut rx) = trpl::channel();
 
-        let val = String::from("hi");
-        tx.send(val).unwrap();
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("async"),
+            String::from("world!"),
+        ];
 
-        let received = rx.recv().await.unwrap();
-        println!("received: {}", received);
+        for val in vals {
+            tx.send(val).unwrap();
+            trpl::sleep(Duration::from_millis(500)).await;
+        }
 
+        while let Some(value) = rx.recv().await {
+            println!("received '{value}'");
+        }
     });
 }
 
-// listing 17-9
+// listing 17-10
+// reciever awaits messages from a channel and does not exit until the channel is closed
