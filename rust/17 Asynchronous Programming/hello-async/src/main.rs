187b558567c8501059bd464a38608952d1ef0aaa
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-10-31 21:08:56 -0700

    17-6 listing

--- rust/17 Asynchronous Programming/hello-async/src/main.rs
+++ rust/17 Asynchronous Programming/hello-async/src/main.rs
@@ -1,35 +1,21 @@
 extern crate trpl; // required for mdbook test
-use trpl::{Either, Html};
 
-async fn page_title(url: &str) -> (&str, Option<String>) {
-    let response_text = trpl::get(url).await.text().await;
-    let title = Html::parse(&response_text)
-        .select_first("title")
-        .map(|title| title.inner_html());
-    (url, title)
-}
-
+use std::time::Duration;
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-
     trpl::run(async {
-        
-        let title_fut_1 = page_title(&args[1]);
-        let title_fut_2 = page_title(&args[2]);
+        trpl::spawn_task(async {
+            for i in 1..10 {
+                println!("hi number {i} from first task!");
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        });
 
-        let (url, maybe_title) = 
-            match trpl::race(title_fut_1, title_fut_2).await {
-                Either::Left(left) => left,
-                Either::Right(right) => right,
-            };
-
-        println!("{url} returned first");
-        match maybe_title {
-            Some(title) => println!("The page title is {title}"),
-            None => println!("It had no title"),
+        for i in 1..5 {
+            println!("hi number {i} from second task!");
+            trpl::sleep(Duration::from_millis(500)).await;
         }
     })
 }
-// cargo run -- https://www.rust-lang.org https://www.example.com
-// Listing 17-5
+
+// listing 17-6
