602f10803e3e4c8f1fed273345541f9fafa17ec9
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-11-01 08:24:41 -0700

    await each async block immediately after defining it.

--- rust/17 Asynchronous Programming/hello-async/src/main.rs
+++ rust/17 Asynchronous Programming/hello-async/src/main.rs
@@ -10,6 +10,7 @@ fn main() {
                 trpl::sleep(Duration::from_millis(500)).await;
             }
         };
+        fut1.await;
 
         let fut2 = async {
             for i in 1..5 {
@@ -17,8 +18,10 @@ fn main() {
                 trpl::sleep(Duration::from_millis(500)).await;
             }   
         };
+        fut2.await;
 
-        trpl::join(fut1, fut2).await;
+        //trpl::join(fut1, fut2).await;
+    
     });
 }
 
