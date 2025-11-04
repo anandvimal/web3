4854329cee5419ede6185c785375a6cbd3b74acf
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-11-04 09:36:17 -0800

    Update main.rs

--- rust/17 Asynchronous Programming/hello-async/src/main.rs
+++ rust/17 Asynchronous Programming/hello-async/src/main.rs
@@ -1,24 +1,17 @@
 extern crate trpl; // required for mdbook test
 
 //use std::pin::{Pin, pin};
-use std::time::Duration;
+use std::{thread, time::Duration};
 
 fn main() {
     trpl::run(async {
-        let slow = async {
-            println!("slow started");
-            trpl::sleep(Duration::from_millis(100)).await;
-            println!("slow finished");
-        };
-
-        let fast = async {
-            println!("fast started");
-            trpl::sleep(Duration::from_millis(50)).await;
-            println!("fast finished");
-        };
-
-        trpl::race(slow, fast).await;
+        // we will call 'slow' here later.
     });
 }
 
-// Listing 17-21: Using race to get the result of whichever future finishes first
+fn slow(name: &str, ms: u64) {
+    thread::sleep(Duration::from_millis(ms));
+    println!("{name} ran for {ms} ms");
+}
+
+// Listing 17-22: Using thread::sleep to simulate slow operations
