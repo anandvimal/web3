0e58bc4354a21c017fc927413a8d9d7aea342852
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-11-04 10:09:25 -0800

    Listing 17-24: Using sleep to let operations switch off making progress

--- rust/17 Asynchronous Programming/hello-async/src/main.rs
+++ rust/17 Asynchronous Programming/hello-async/src/main.rs
@@ -5,21 +5,29 @@ use std::{thread, time::Duration};
 
 fn main() {
     trpl::run(async {
+        let one_ms = Duration::from_millis(1);
+
         let a = async {
             println!("starting a");
             slow("a", 30);
+            trpl::sleep(one_ms).await;
             slow("a", 10);
+            trpl::sleep(one_ms).await;
             slow("a", 20);
-            trpl::sleep(Duration::from_millis(50)).await;
+            trpl::sleep(one_ms).await;
             println!("finished a");
         };
 
         let b = async {
             println!("starting b");
             slow("b", 75);
+            trpl::sleep(one_ms).await;
             slow("b", 10);
+            trpl::sleep(one_ms).await;
             slow("b", 15);
+            trpl::sleep(one_ms).await;
             slow("b", 350);
+            trpl::sleep(one_ms).await;
             trpl::sleep(Duration::from_millis(50)).await;
             println!("finished b");
         };
@@ -33,4 +41,4 @@ fn slow(name: &str, ms: u64) {
     println!("{name} ran for {ms} ms");
 }
 
-// Listing 17-23: Using thread::sleep to simulate slow operations
+// Listing 17-24: Using sleep to let operations switch off making progress
