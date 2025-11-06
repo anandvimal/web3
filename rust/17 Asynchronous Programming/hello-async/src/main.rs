fbdcb4a773efcad89236a6c628b2bd765caea084
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-11-05 21:23:48 -0800

    Listing 17-35: Sending messages through tx with an async delay without making get_messages an async function

--- rust/17 Asynchronous Programming/hello-async/src/main.rs
+++ rust/17 Asynchronous Programming/hello-async/src/main.rs
@@ -9,8 +9,8 @@ fn main() {
 
         while let Some(result) = messages.next().await {
             match result {
-                Ok(message) => println!("Received: {message}"),
-                Err(reason) => eprintln!("Program : {reason:?}"),
+                Ok(message) => println!("Message: {message}"),
+                Err(reason) => eprintln!("Problem : {reason:?}"),
             }
         }
     });
@@ -19,12 +19,19 @@ fn main() {
 fn get_messages() -> impl Stream<Item = String> {
     let (tx, rx) = trpl::channel();
 
-    let messages = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
-    for message in messages {
-        tx.send(format!("Message: '{message}'")).unwrap();
-    }
+    trpl::spawn_task(async move {
+        let messages = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+        for (index, message) in messages.into_iter().enumerate() {
+            let time_to_sleep = if index % 2 == 0 {100} else {300};
+            trpl::sleep(Duration::from_millis(time_to_sleep)).await;
+
+            tx.send(format!("Message '{message}'")).unwrap();
+
+        }
+    });
+    
 
     ReceiverStream::new(rx)
 }
 
-// Listing 17-34: Using the StreamExt::timeout method to set a time limit on the items in a stream
+// Listing 17-35: Sending messages through tx with an async delay without making get_messages an async function
