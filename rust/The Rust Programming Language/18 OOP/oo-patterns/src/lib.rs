f1951cba983bec56c9234f70af4e9920666b476b
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-11-10 11:42:05 -0800

    18-1 struct that maintains a list of integers and average of the items in the collection

--- rust/The Rust Programming Language/18 OOP/oo-patterns/src/lib.rs
+++ rust/The Rust Programming Language/18 OOP/oo-patterns/src/lib.rs
@@ -1,3 +1,8 @@
+pub struct AveragedCollection {
+    list: Vec<i32>,
+    average: f64,
+}
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
@@ -12,3 +17,7 @@ mod tests {
         assert_eq!(result, 4);
     }
 }
+
+
+// Listing 18-1: An AveragedCollection struct that maintains a list of integers and the average of the items in the collection
+
