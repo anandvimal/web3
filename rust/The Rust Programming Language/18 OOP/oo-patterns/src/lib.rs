3d17de01b55a65ad88caa5dc3cf165b72a31bb5d
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-11-10 11:43:10 -0800

    Listing 18-2: Implementations of the public methods add, remove, and average on AveragedCollection

--- rust/The Rust Programming Language/18 OOP/oo-patterns/src/lib.rs
+++ rust/The Rust Programming Language/18 OOP/oo-patterns/src/lib.rs
@@ -3,6 +3,35 @@ pub struct AveragedCollection {
     average: f64,
 }
 
+impl AveragedCollection{
+    
+    pub fn add(&mut self, value:i32){
+        self.list.push(value);
+        self.update_average();
+    }
+
+    pub fn remove(&mut self) -> Option<i32> {
+        let result = self.list.pop()
+        match result {
+            Some(value) => {
+                self.update_average();
+                Some(value)
+            }
+            None => None,
+        }
+    }
+
+    pub fn average(&self) -> f64 {
+        self.average
+    }
+
+    fn update_average(&mut self) {
+        let total: i32 = self.list.iter().sum();
+        self.average = total as f64 / self.list.len() as f64;
+    }
+
+}
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
@@ -19,5 +48,4 @@ mod tests {
 }
 
 
-// Listing 18-1: An AveragedCollection struct that maintains a list of integers and the average of the items in the collection
-
+// Listing 18-2: Implementations of the public methods add, remove, and average on AveragedCollection.
