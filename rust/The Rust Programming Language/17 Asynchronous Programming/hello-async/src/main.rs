2aaae9988a2561bb979c8e831baf27519f7531af
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-11-09 22:12:01 -0800

    Listing 17-41: Using the std::thread APIs instead of the async trpl APIs for the get_intervals function

--- rust/The Rust Programming Language/17 Asynchronous Programming/hello-async/src/main.rs
+++ rust/The Rust Programming Language/17 Asynchronous Programming/hello-async/src/main.rs
@@ -44,20 +44,21 @@ fn get_messages() -> impl Stream<Item = String> {
 fn get_intervals() -> impl Stream<Item = u32> {
     let (tx, rx) = trpl::channel();
 
-    trpl::spawn_task(async move {
+    // This is *not* `trpl::spawn` but `std::thread::spawn`!
+    thread::spawn(move || {
         let mut count = 0;
         loop {
-            trpl::sleep(Duration::from_millis(1)).await;
+            // Likewise, this is *not* `trpl::sleep` but `std::thread::sleep`!
+            thread::sleep(Duration::from_millis(1));
             count += 1;
 
             if let Err(send_error) = tx.send(count) {
-                eprintln!("Cound not send interval {count}: {send_error}");
-                break
-            }
+                eprintln!("Could not send interval {count}: {send_error}");
+                break;
+            };
         }
     });
 
     ReceiverStream::new(rx)
 }
-
-// Listing 17-40: Handling errors and shutting down the loops
+// Listing 17-41: Using the std::thread APIs instead of the async trpl APIs for the get_intervals function
