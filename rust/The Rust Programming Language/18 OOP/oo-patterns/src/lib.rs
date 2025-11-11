1a5b494112350345e24c29278fa41feec67cb8de
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-11-10 21:33:44 -0800

    remove boiler plate code from lib.rs

--- rust/The Rust Programming Language/18 OOP/oo-patterns/src/lib.rs
+++ rust/The Rust Programming Language/18 OOP/oo-patterns/src/lib.rs
@@ -3,19 +3,12 @@ pub trait Draw {
 }
 // Listing 18-3: Definition of the Draw trait
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    
 }
 
 
