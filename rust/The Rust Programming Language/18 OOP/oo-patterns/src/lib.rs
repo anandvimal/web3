d204e2dfa3d87967fa51b3ac6e6c8f3f0b065653
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-11-10 22:04:34 -0800

    Listing 18-4: Definition of the Screen struct with a components field holding a vector of trait objects that implement the Draw trait

--- rust/The Rust Programming Language/18 OOP/oo-patterns/src/lib.rs
+++ rust/The Rust Programming Language/18 OOP/oo-patterns/src/lib.rs
@@ -1,14 +1,18 @@
 pub trait Draw {
     fn draw(&self);
 }
-// Listing 18-3: Definition of the Draw trait
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+// Listing 18-4: Definition of the Screen struct with a components field holding a vector of trait objects that implement the Draw trait
 
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    
+
 }
 
 
