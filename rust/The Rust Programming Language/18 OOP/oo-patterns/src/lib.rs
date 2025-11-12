778603a62b9c68bca696bc6dd298527c1736e695
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-11-11 17:53:33 -0800

    Listing 18-12: Definition of a Post struct and a new function that creates a new Post instance, a State trait, and a Draft struct

--- rust/The Rust Programming Language/18 OOP/oo-patterns/src/lib.rs
+++ rust/The Rust Programming Language/18 OOP/oo-patterns/src/lib.rs
@@ -1,10 +1,27 @@
+pub struct Post {
+    state: Option<Box<dyn State>>,
+    content: String,
+}
 
+impl Post {
+    pub fn new() -> Post {
+        Post {
+            state: Some(Box::new(Draft {})),
+            content: String::new(),
+        }
+    }
+}
+
+trait State {}
+
+struct Draft {}
+
+impl State for Draft {}
+
+// Listing 18-12: Definition of a Post struct and a new function that creates a new Post instance, a State trait, and a Draft struct
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
 }
-
-
-
