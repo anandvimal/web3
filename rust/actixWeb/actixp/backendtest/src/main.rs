ff33fb706cc2a2ce664115593fe5a485999f072a
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-11-20 16:04:58 -0800

    Update main.rs

--- rust/actixWeb/actixp/backendtest/src/main.rs
+++ rust/actixWeb/actixp/backendtest/src/main.rs
@@ -1,5 +1,15 @@
 use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
 
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
 fn main() {
     println!("Hello, world!");
 }
