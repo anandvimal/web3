252107766bdb4df1a2abc224723660a3d2234b35
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-11-20 16:12:24 -0800

    Update main.rs

--- rust/actixWeb/actixp/backendtest/src/main.rs
+++ rust/actixWeb/actixp/backendtest/src/main.rs
@@ -10,6 +10,10 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
 fn main() {
     println!("Hello, world!");
 }
