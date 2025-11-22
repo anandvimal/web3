6f5ab5d8978451e4656fb42395b16a722930d14f
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-11-22 14:41:21 -0800

    Update main.rs

--- rust/actixWeb/actixp/backendtest/src/main.rs
+++ rust/actixWeb/actixp/backendtest/src/main.rs
@@ -1,28 +1,30 @@
-use actix_web::{App, HttpServer, get, web};
+use actix_web::{App, HttpServer, web};
 
-// This struct represents state.
-struct AppState {
-    app_name: String,
+struct AppStateWithCounter {
+    counter: Mutex<i32>, // <- mutex is necessary to mutate safely across threads
 }
 
-#[get("/")]
-async fn index(data: web::Data<AppState>) -> String {
-    let app_name = &data.app_name; // <- get app_name
-    format!("Hello, {app_name}!") // <- response with app_name
+async fn index(data: web::Data<AppStateWithCounter>) -> String {
+    let mut counter = data.counter.lock().unwrap(); // <- get counter's MutexGuard
+    *counter += 1; // <- access counter inside MutexGuard
+
+    format!("Request number: {counter}") // <- response with count
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    // Note: web:Data created _outside_ HttpServer::new closure
+    let counter = web::Data::new(AppStateWithCounter {
+        counter: Mutex::new(0),
+    });
+
+    HttpServer::new(move || {
+        // move counter into the closure
         App::new()
-            .app_data(web::Data::new(AppState {
-                app_name: String::from("Actix-Web"),
-            }))
-            .service(index)
+            .app_data(counter.clone()) // <- retister the created data
+            .route("/", web::get().to(index))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
-
-// https://actix.rs/docs/application#state
